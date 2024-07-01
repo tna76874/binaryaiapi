e8585256729aad5b9a9edfda43216e4f2561a665
lmh <dev@hilberg.eu>
2024-07-01 06:48:37 +0200

    add brightness check

--- detect.py
+++ detect.py
@@ -25,7 +25,6 @@ tf.config.set_visible_devices([], 'GPU')
 logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 
 
-
 def none_on_exception(func):
     @wraps(func)
     def wrapper(*args, **kwargs):
@@ -35,6 +34,26 @@ def none_on_exception(func):
             # print(f'Error: {e}')
             return None
     return wrapper
+
+class BrightnessCheck:
+    def __init__(self, file_buffer, min_brightness=210, max_brightness=252):
+        self.file_buffer = file_buffer
+        self.min_brightness = min_brightness
+        self.max_brightness = max_brightness
+        self.brightness = self._calculate_brightness()
+
+    def _calculate_brightness(self):
+        image = Image.open(BytesIO(self.file_buffer))
+        greyscale_image = image.convert('L')
+        np_image = np.array(greyscale_image)
+        brightness = np.mean(np_image)
+        return brightness
+
+    def check(self):
+        if self.min_brightness <= self.brightness <= self.max_brightness:
+            return True
+        else:
+            return False
 
 class AspectRatioCheck:
     def __init__(self, file_buffer, tolerance=0.5):
@@ -85,17 +104,19 @@ class FileLoader:
         self._generate_pages()
         self._classify_pages()
         self._check_aspect_ratio()
+        self._check_brightness()
         
         return self
     
     @none_on_exception
     def _get_classify_results(self):
         if len(self.pages) == 0:
-            return {'ratio': None, 'cnn': None, 'blur': None, 'pass' : False}
+            return {'brightness': None, 'ratio': None, 'cnn': None, 'blur': None, 'pass' : False}
 
         cnn_status = [page.get('cnn', {}).get('status') for page in self.pages]
         blur_status = [page.get('blur', {}).get('status') for page in self.pages]
         ratio_status = [page.get('ratio', {}).get('status') for page in self.pages]
+        brightness_status = [page.get('brightness', {}).get('status') for page in self.pages]
         
         # CNN Mean Prediction
         cnn_prediction_values = np.array([page.get('cnn', {}).get('prediction') for page in self.pages if page.get('cnn', {}).get('prediction') is not None], dtype=float)
@@ -105,13 +126,18 @@ class FileLoader:
         blur_variance_values = np.array([page.get('blur', {}).get('variance') for page in self.pages if page.get('blur', {}).get('variance') is not None], dtype=float)
         blur_variance_mean = np.mean(blur_variance_values) if blur_variance_values.size > 0 else None
         
+        # Brightness Mean
+        brightness_values = np.array([page.get('brightness', {}).get('brightness') for page in self.pages if page.get('brightness', {}).get('brightness') is not None], dtype=float)
+        brightness_mean = np.mean(brightness_values) if brightness_values.size > 0 else None
+        
         cnn_pass = all(cnn_value == True for cnn_value in cnn_status)
         blur_pass = all(blur_value == False for blur_value in blur_status)
         ratio_pass = all(ratio_value == True for ratio_value in ratio_status)
+        brightness_pass = all(brightness_value == True for brightness_value in brightness_status)
         
-        all_pass = cnn_pass and blur_pass and ratio_pass
+        all_pass = cnn_pass and blur_pass and ratio_pass and brightness_pass
 
-        results = {'ratio' : ratio_pass, 'cnn': cnn_pass, 'blur': blur_pass, 'pass' : all_pass, 'values' : {'cnn' : cnn_prediction_mean, 'blur' : blur_variance_mean }}
+        results = {'brightness' : brightness_pass, 'ratio' : ratio_pass, 'cnn': cnn_pass, 'blur': blur_pass, 'pass' : all_pass, 'values' : {'cnn' : cnn_prediction_mean, 'blur' : blur_variance_mean, 'brightness' : brightness_mean}}
         
         return results
     
@@ -132,7 +158,13 @@ class FileLoader:
             ratio = AspectRatioCheck(page['buffer'], tolerance=self.ratio_tolerance)
             ratio_status = ratio.check()
             self.pages[idx].update({'ratio' : {'status' : ratio_status, 'normed_ratio' : ratio.normed_ratio}})
-
+            
+    @none_on_exception
+    def _check_brightness(self):
+        for idx, page in enumerate(self.pages):
+            brightness = BrightnessCheck(page['buffer'])
+            brightness_status = brightness.check()
+            self.pages[idx].update({'brightness' : {'status' : brightness_status, 'brightness' : brightness.brightness}})
     
     @none_on_exception
     def _generate_pages_from_pdf(self):
